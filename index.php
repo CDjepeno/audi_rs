ecfdbea56a362b3ea6779987d3d7f556e166e77a
CDjepeno <djepeno.christopher@gmail.com>
2020-08-21 01:15:41 +0200

    modification du fichier d'autoload

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 require_once "vendor/autoload.php";
 
-require_once "utilities/config.php";
-require_once "utilities/Database.php";
-require_once "utilities/Session.php";
-
-
-Autoloader::class();
-
-require_once "utilities/AbstractController.php";
-require_once "controllers/DefaultController.php";
-require_once "controllers/ProductController.php";
-require_once "controllers/UsersController.php";
-require_once "controllers/ContactController.php";
-require_once "controllers/OrderController.php";
 
 
 // Ont vérifie ici l'action envoyer par l'url pour effectuer une action précise
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,16 +1,27 @@
 <?php
 
-class Autoloader {
 
-    public static function Class() {   
+    function my_autoloader($className) {   
+
+        if(file_exists("utilities/$className.php")) {
+
+            require_once "utilities/$className.php";
+
+        } elseif(file_exists("controllers/$className.php")) {
+
+            require_once "controllers/$className.php";
+
+        } elseif(file_exists("modeles/$className.php")) {
+
+            require_once "modeles/$className.php";
+
+        } else {
+            throw new Exception("Cette class $className n'existe pas");
+        }   
         
-        spl_autoload_register(function($className){
-        require_once "modeles/$className.php";});
-    
     }
+    spl_autoload_register("my_autoloader");
 
-
-}
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
