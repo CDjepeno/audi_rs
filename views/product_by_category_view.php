472da715be0f18e5a278faa027e6ab5d4cfca2fc
CDjepeno <djepeno.christopher@gmail.com>
2020-10-30 20:29:42 +0100

    Modification partie front en sass

--- views/product_by_category_view.php
+++ views/product_by_category_view.php
@@ -1,18 +1,24 @@
 <?php include_once "header_view.php" ?>
 
-<div class="container_cat">
+<div class="container">
     <?php foreach ($products as $p): ?>
-        <article id="product_cat">
-            <a href="index.php?class=product&action=show&id=<?= $p->getId() ?>"> <h2><?= $p->getName() ?></h2></a>
-            <a href="index.php?class=product&action=show&id=<?= $p->getId() ?>">
-            <img src="assets/img/product/<?= htmlspecialchars ($p->getpath()) ?>" alt="Audi" />
-            </a>
-            <br>
-            <span>Tarif: <?= $p->getBuy_price() ?> € HT</span>
-            <p><?= substr($p->getDescription(), 0, 50)  ?>...</p>
-            <a class="btn" href="index.php?class=product&action=show&id=<?= $p->getId() ?>" >Voir le produit</a>
-        </article>
-    <?php endforeach ?>
+        <section id="cars">
+            <article id="product">
+                <a href="index.php?class=product&action=show&id=<?= $p->getId() ?>"> <h2><?= $p->getName() ?></h2></a>
+                <section class="img">
+                    <a href="index.php?class=product&action=show&id=<?= $p->getId() ?>">
+                    <img src="assets/img/product/<?= htmlspecialchars ($p->getpath()) ?>" alt="Audi" />
+                </section>
+                </a>
+                <br>
+                <section class="description">
+                    <span>Tarif: <?= $p->getBuy_price() ?> € HT</span>
+                    <p><?= substr($p->getDescription(), 0, 50)  ?>...</p>
+                </section>
+                <a class="btn" href="index.php?class=product&action=show&id=<?= $p->getId() ?>" >Voir le produit</a>
+            </article>
+        <?php endforeach ?>
+    </section>
 </div>
 
 <?php include_once "footer_view.php" ?>
